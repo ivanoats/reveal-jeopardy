22fe999bd416e88812d6bc61df60f17768fc87ac
Ivan Storck <ivanoats@gmail.com>
2014-10-28 10:09:00 -0700

    server js working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
-
-app.listen(process.env.PORT || 3000);
-console.log('jeopardy express server started');
+app.use(express.static(__dirname));
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('jeopardy express server started on ' + port);
